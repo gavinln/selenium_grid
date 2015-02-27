fbe9b91c3ca0299fcb5bd4e0d25dc90be41db833
Gavin Noronha <gavinln@hotmail.com>
2015-02-27 03:37:52 -0800

    improved setup of zsh

--- scripts/zsh_setup.sh
+++ scripts/zsh_setup.sh
@@ -2,8 +2,12 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-#wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
-chsh -s /bin/zsh
+# setup ohmyzsh
+if [ ! -d ~/.oh-my-zsh ]; then
+    wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+    chsh -s /bin/zsh
+fi
+
 
 # setup z.sh to help traverse directories
 cp $DIR/z.sh ~/z.sh
@@ -14,3 +18,7 @@ ZSH_SETUP="source ~/z.sh"
 if ! grep -qe "$ZSH_SETUP" $ZSHRC; then
     echo "$ZSH_SETUP" >> $ZSHRC
 fi
+
+# setup ohmyzsh plugins
+sed -i "s/plugins=(git)/plugins=(git fabric)/" $ZSHRC
+
