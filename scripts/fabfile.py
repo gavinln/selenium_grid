a08fae6f8584f65b3f4e8e9091863870f8f9b79d
Gavin Noronha <gavinln@hotmail.com>
2015-03-08 20:52:08 -0700

    Updated to put all fab commands in one file

--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -9,18 +9,30 @@ import shutil
 script_dir = os.path.dirname(__file__)
 
 
-def get_jenkins_root():
+def get_docker_path(dockerDir):
     return os.path.normpath(
-        os.path.join(script_dir, '..', 'docker', 'jenkins'))
+        os.path.join(script_dir, '..', 'docker', dockerDir))
+
+
+def get_jenkins_root():
+    return get_docker_path('jenkins')
 
 
 def get_nodejs_root():
+    return get_docker_path('nodejs')
+
+
+def get_selenium_root():
+    return get_docker_path('selenium')
+
+
+def get_python_root():
     return os.path.normpath(
-        os.path.join(script_dir, '..', 'docker', 'nodejs'))
+        os.path.join(script_dir, '..', 'python'))
 
 
 @task
-def jenkins_start():
+def jk_master_start():
     ''' start jenkins master '''
     with lcd(get_jenkins_root()):
         local('sudo fig up -d ciserver')
@@ -28,30 +40,66 @@ def jenkins_start():
 
 
 @task
-def jenkins_stop():
+def jk_master_stop():
     ''' stop jenkins master '''
     with lcd(get_jenkins_root()):
         local('sudo fig stop ciserver')
 
 
 @task
-def nodejs_start():
-    ''' start nodejs jenkins slave '''
+def jk_slave_start():
+    ''' start jenkins nodejs slave '''
     with lcd(get_nodejs_root()):
         local('sudo fig up -d cislave')
 
 
 @task
-def nodejs_stop():
-    ''' stop nodejs jenkins slave '''
+def jk_slave_stop():
+    ''' stop jenkins nodejs slave '''
     with lcd(get_nodejs_root()):
         local('sudo fig stop cislave')
 
 
 @task
-def status():
-    ''' stop hub & node '''
+def jk_status():
+    ''' status of jenkins master & slave '''
     with lcd(get_jenkins_root()):
         local('sudo fig ps')
     with lcd(get_nodejs_root()):
         local('sudo fig ps')
+
+
+@task
+def sl_grid_start():
+    ''' start selenium hub & node '''
+    with lcd(get_selenium_root()):
+        local('sudo fig up -d hub node')
+        local('sudo fig ps node')
+
+
+@task
+def sl_grid_stop():
+    ''' stop selenium hub & node '''
+    with lcd(get_selenium_root()):
+        local('sudo fig stop hub node')
+
+
+@task
+def sl_standalone_start():
+    ''' start selenium standalone '''
+    with lcd(get_selenium_root()):
+        local('sudo fig up -d standalone')
+
+
+@task
+def sl_standalone_stop():
+    ''' stop selenium standalone '''
+    with lcd(get_selenium_root()):
+        local('sudo fig stop standalone')
+
+
+@task
+def run_test():
+    ''' run python test scripts '''
+    with lcd(get_python_root()):
+    	local('python selenium_test.py')
